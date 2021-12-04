b23e27040618d4947e73819a887317eca92618be
ewalk86 <ethanwalker86@gmail.com>
2021-12-03 17:21:31 -0700

    New report for household letter

---
+++ render_reports.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(rmarkdown)
+
+file_path <- c("C:/Users/ethan.walker/Box/Ethan Walker UM/R/EldersAIR/")
+
+analysis_data <- read_rds(paste0(file_path, "Output/elders_cleaned_dataset_by_visit.rds")) %>% 
+  mutate(cohort = factor(cohort, levels = c("2015", "2016", "2017", "2018", "2019"))) %>% 
+  filter(area == "NPT") %>% 
+  filter(pm_mean_winter < 200)
+
+for (home in unique(analysis_data$home)){
+  home_data <- analysis_data[analysis_data$home == home,]
+  render("letter_results.rmd", output_file = paste0('report.', home, '.docx'))    
+}
