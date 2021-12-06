99e1e136a6ba1d10f79dd2c89aa902cdaef568a3
ewalk86 <ethanwalker86@gmail.com>
2021-12-06 15:48:03 -0700

    updated letter template

--- render_reports.R
+++ render_reports.R
@@ -8,7 +8,12 @@ analysis_data <- read_rds(paste0(file_path, "Output/elders_cleaned_dataset_by_vi
   filter(area == "NPT") %>% 
   filter(pm_mean_winter < 200)
 
-for (home in unique(analysis_data$home)){
-  home_data <- analysis_data[analysis_data$home == home,]
-  render("letter_results.rmd", output_file = paste0('report.', home, '.docx'))    
+#for (home in unique(analysis_data$home)){
+ # home_data <- analysis_data[analysis_data$home == home,]
+  #render("letter_results.rmd", output_file = paste0('report.', home, '.docx'))    
+#}
+
+for (adult_id_char in unique(analysis_data$adult_id_char)){
+  adult_data <- analysis_data[analysis_data$adult_id_char == adult_id_char,]
+  render("letter_results.rmd", output_file = paste0('report.', adult_id_char, '.docx'))    
 }
