75522511e31091c45a96c0a64466b48f1ecd5662
ewalk86 <ethanwalker86@gmail.com>
2021-12-09 11:45:07 -0700

    results report updates

--- render_reports.R
+++ render_reports.R
@@ -6,7 +6,14 @@ file_path <- c("C:/Users/ethan.walker/Box/Ethan Walker UM/R/EldersAIR/")
 analysis_data <- read_rds(paste0(file_path, "Output/elders_cleaned_dataset_by_visit.rds")) %>% 
   mutate(cohort = factor(cohort, levels = c("2015", "2016", "2017", "2018", "2019"))) %>% 
   filter(area == "NPT") %>% 
-  filter(pm_mean_winter < 200)
+  filter(pm_mean_winter < 200) %>% 
+  mutate("Winter:" = factor(winter_id, levels = c(1, 2),
+                            labels = c("Pre-Intervention", "Post-Intervention")),
+         treatment = factor(treatment, 
+                            levels = c("Placebo", "Education", "Filter"),
+                            labels = c("Placebo Group", "Education Group", "Filter Group")),
+         pm_mean_winter = round(pm_mean_winter, digits = 0),
+         mp_mean_winter = round(mp_mean_winter, digits = 0))
 
 #for (home in unique(analysis_data$home)){
  # home_data <- analysis_data[analysis_data$home == home,]
