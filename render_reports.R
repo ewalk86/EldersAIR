798d02e6171733f5e2800e7a047e5562b4749953
ewalk86 <ethanwalker86@gmail.com>
2021-12-17 15:13:57 -0700

    Smaller updates to results letters

--- render_reports.R
+++ render_reports.R
@@ -6,9 +6,9 @@ file_path <- c("C:/Users/ethan.walker/Box/Ethan Walker UM/R/EldersAIR/")
 analysis_data <- read_rds(paste0(file_path, "Output/elders_cleaned_dataset_by_visit.rds")) %>% 
   mutate(cohort = factor(cohort, levels = c("2015", "2016", "2017", "2018", "2019"))) %>% 
   filter(area == "NPT") %>% 
-  filter(pm_mean_winter < 200) %>% 
+  #filter(pm_mean_winter < 200) %>% 
   mutate("Winter:" = factor(winter_id, levels = c(1, 2),
-                            labels = c("Pre-Intervention", "Post-Intervention")),
+                            labels = c("1st Winter/Pre-Intervention", "2nd Winter/Post-Intervention")),
          treatment = factor(treatment, 
                             levels = c("Placebo", "Education", "Filter"),
                             labels = c("Placebo Group", "Education Group", "Filter Group")),
